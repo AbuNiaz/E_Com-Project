cfe645c09e5a1f9fa894414bcedf128b5e4b3249
Abu Niaz <72647971+AbuNiaz@users.noreply.github.com>
2021-09-25 18:16:57 +0600

    Add files via upload

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -16,7 +16,7 @@ class LoginPage extends StatelessWidget {
           onPressed: () {
             Navigator.pop(context);
           },
-          icon: Icon(Icons.arrow_back_ios),
+          icon: Icon(Icons.arrow_back_outlined),
           iconSize: 20,
           color: Colors.black,
         ),
@@ -41,14 +41,23 @@ class LoginPage extends StatelessWidget {
                       ),
                     ),
                     SizedBox(
-                      height: 10,
+                      height: 60,
                     ),
                   ],
                 ),
                 Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 40),
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    children: <Widget>[
+                      InputFile(lable: "Email"),
+                      InputFile(lable: "Password", obscureText: true)
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 30),
                   child: Container(
-                    padding: EdgeInsets.only(top: 30, left: 3),
+                    padding: EdgeInsets.only(top: 0, left: 0),
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(50),
                         border: Border(
@@ -75,7 +84,18 @@ class LoginPage extends StatelessWidget {
                       ),
                     ),
                   ),
-                )
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("Dont have an account?"),
+                    Text(
+                      "Sign Up",
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
               ],
             ))
           ],
@@ -83,4 +103,33 @@ class LoginPage extends StatelessWidget {
       ),
     );
   }
+}
+
+Widget InputFile({lable, obscureText = false}) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        lable,
+        style: TextStyle(
+            fontSize: 15, fontWeight: FontWeight.w400, color: Colors.black38),
+      ),
+      SizedBox(
+        height: 5,
+      ),
+      TextField(
+        obscureText: obscureText,
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey.shade400),
+            ),
+            border: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey.shade400))),
+      ),
+      SizedBox(
+        height: 10,
+      )
+    ],
+  );
 }
