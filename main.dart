5fab368991663c029e84b41ba22b2523e361340a
Abu Niaz <niaz35-507@diu.edu.bd>
2021-11-18 20:23:46 +0600

    update 18-11

--- main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:e_com/Main_Page/home.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: HomePage(),
-  ));
-}
